413a871860c0012691e5b034d3a864eb62f11bf4
Cody Brubaker <cody.brubaker@email.com>
2017-02-17 09:36:51 -0800

    add custom field to devise and set up login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   root to: "landing#index"
+
+  resources :products do
+    resources :reviews
+  end
 end
