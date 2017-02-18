1f0ca4473f020b1c00c125d8f4d60d89f464872a
Cody Brubaker <cody.brubaker@email.com>
2017-02-17 16:38:26 -0800

    commit for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,8 @@ Rails.application.configure do
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
-  }
+  },
+  :s3_host_name => "s3-us-west-2.amazonaws.com",
 }
   # Settings specified here will take precedence over those in config/application.rb.
 
